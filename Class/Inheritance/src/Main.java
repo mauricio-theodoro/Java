92ce79de7c2e45ec183bc2b3b6a1a54e9c4af4c9
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2024-05-12 13:07:42 -0300

    Add files via upload

---
+++ Class/Inheritance/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Mouse2 m2 = new Mouse2();
+        m2.leftClick();
+        m2.rightClick();
+        m2.scrollDown();
+        m2.scrollUp();
+
+        m2.connect();
+
+
+    }
+}
