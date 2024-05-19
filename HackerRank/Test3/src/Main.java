b86285f5c36229b9b0519179a37252326624a571
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2024-05-18 21:06:59 -0300

    Add files via upload

---
+++ HackerRank/Test3/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        if(N % 2 == 1){
+            System.out.println("Weird");
+        }else if(N % 2 == 0 && N> 1 && N <6){
+            System.out.println("Not Weird");
+        }else if(N % 2 == 0 && N > 5 && N < 21){
+            System.out.println("Weird");
+        }else
+            System.out.println("Not Weird");
+
+    }
+}
