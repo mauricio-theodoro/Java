3f04affbc3bcb74630013716bc661378ca38faac
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2025-04-06 11:57:34 -0300

    Add files via upload

--- arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/api/TesteFabricaController.java
@@ -2,15 +2,22 @@ package io.github.cursodsousa.arquiteturaspring.api;
 
 import io.github.cursodsousa.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
+    //@Qualifier("motorTurbo")
     @Autowired
+    @Aspirado
     private Motor motor;
 
+    @PostMapping
     public CarroStatus ligarCarro(@RequestBody Chave chave) {
         var carro = new HondaHRV(motor);
         return carro.darIgnicao(chave);
