8393c109eb0cde46c3b1221c107da91f3b2c2d5c
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2025-04-06 13:32:37 -0300

    Add files via upload

---
+++ arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,36 @@
+package io.github.cursodsousa.arquiteturaspring.todos;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+    private TodoValidator validator;
+    private MailSender mailSender;
+
+    public TodoService(TodoRepository todoRepository, TodoValidator validator, MailSender mailSender) {
+        this.repository = todoRepository;
+        this.validator = validator;
+        this.mailSender = mailSender;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo){
+        validator.validar(novoTodo);
+        return  repository.save(novoTodo);
+    }
+
+    public void atualizarStatus(TodoEntity todo) {
+        repository.save(todo);
+        String status  = todo.getConcluido() == Boolean.TRUE ? "Concluido" : "Não concluido";
+        mailSender.enviar("Todo " + todo.getDescricao() + "foi atualizado para " + status);
+    }
+
+    //ou ele retorna o id ou retorna nulo
+    public TodoEntity buscarPorId(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+
+}
