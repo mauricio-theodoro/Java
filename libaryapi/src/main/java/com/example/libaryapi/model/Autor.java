aaba6989aada9ffbd72ebd2026933a6300683a59
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2025-04-07 23:36:02 -0300

    Add files via upload

---
+++ libaryapi/src/main/java/com/example/libaryapi/model/Autor.java
@@ -0,0 +1,30 @@
+package com.example.libaryapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name ="autor", schema ="public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate Nascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+}
