b6189026f422f096d29da919c523ac2fac11221d
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2025-04-08 07:20:28 -0300

    Atualizar o DatabaseConfiguration.java

--- libaryapi/src/main/java/com/example/libaryapi/config/DatabaseConfiguration.java
+++ libaryapi/src/main/java/com/example/libaryapi/config/DatabaseConfiguration.java
@@ -31,11 +31,7 @@ public class DatabaseConfiguration {
         return ds;
     }
 
-    /**
-     * configuracao Hikary
-     * https://github.com/brettwooldridge/HikariCP
-     * @return
-     */
+
     @Bean
     public DataSource hikariDataSource(){
 
